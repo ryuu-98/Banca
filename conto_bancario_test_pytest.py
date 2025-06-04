1d6d322fd933cba5a3e1fd31e8ffad5cdd2b6c03
ryuu-98 <daarkin98@gmail.com>
2025-06-04 11:09:43 +0200

    Update conto_bancario_test_pytest.py

--- conto_bancario_test_pytest.py
+++ conto_bancario_test_pytest.py
@@ -6,7 +6,7 @@ from conto_bancario import ContoBancario
 # mkdir -p .github/workflows    creare cartella workflows che testa prima di pushare
 
 @pytest.fixture #decoratore
-def conto():
+def test_conto():
     return ContoBancario("Mario Rossi", 100)
 
 
